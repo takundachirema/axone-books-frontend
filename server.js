f38a9734e5317ab893432ca7804a7f0611d569e8
takundachirema <takunda.chirema@alumni.uct.ac.za>
2021-12-14 11:04:12 +0200

    port

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 
 app.use(express.static('prod-server'));
 
+var port = process.env.PORT || 8080
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000');
+app.listen(port, function () {
+  console.log('App listening on port '+port);
 });
